33ed11645b481cebce2ea22822828d006509cb66
LaurieMcCulloch <laurie.mcculloch@ntlworld.com>
2018-02-06 17:22:58 +0000

    deltaDNA SDK Added
    Added deltaDNA SDK v4.7.1

---
+++ Assets/Scripts/PromoImagesTutorial.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DeltaDNA;
+
+namespace TutoralPromoImages
+{
+    public class PromoImagesTutorial : MonoBehaviour
+    {
+        public const string CLIENT_VERSION = "0.0.01";
+
+
+        // Use this for initialization
+        void Start()
+        {
+            // Enter additional configuration here
+            DDNA.Instance.ClientVersion = CLIENT_VERSION;
+            DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
+            DDNA.Instance.Settings.DebugMode = true;
+
+            // Launch the SDK
+            DDNA.Instance.StartSDK(
+                "56922021622026022056772309414071",
+                "https://collect2674dltcr.deltadna.net/collect/api",
+                "https://engage2674dltcr.deltadna.net"
+            );
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
