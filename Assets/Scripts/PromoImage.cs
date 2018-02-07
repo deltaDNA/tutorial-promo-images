933216625d981f16b0a04d05ddecef7071b8df9b
LaurieMcCulloch <laurie.mcculloch@ntlworld.com>
2018-02-07 22:13:34 +0000

    Promo Image Prefab with Engage Calls
    MVP -- Minimal Viable Product,
    Promo Image Prefab added. It can make Engage In-Game requests and update itself from an Image Message contained in the response.
    TODO - Add Game Paramters for deeplinking, rewarding etc..

---
+++ Assets/Scripts/PromoImage.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using DeltaDNA;
+namespace DeltaDNA
+{
+    using JSONObject = System.Collections.Generic.Dictionary<string, object>;
+
+    public class PromoImage : MonoBehaviour
+    {
+
+        // This is the decision point and an engage request parameter
+        // that will be used to target each promo image slot indepently
+        private const string decisionPoint = "promoCheck";
+        public string promoLocation;
+
+        // Texture objects to hold the default and downloaded image 
+        public Texture2D defaultImage;
+        private Texture2D downloadedImage;
+
+        // The image that will display our promo
+        private Image promoImage;
+
+
+        // When the Promo Image is created
+        private void Awake()
+        {
+            // Get Promo Image Panel
+            promoImage = GetComponentInChildren<Image>();
+            
+            // Set Default Promo Image
+            if (defaultImage != null && promoImage != null)
+            {
+                promoImage.sprite = Sprite.Create(defaultImage, new Rect(0.0f, 0.0f, defaultImage.width, defaultImage.height), new Vector2(0.0f, 0.0f), 1.0f);
+            }
+        }
+
+        // When the PromoImage is told to refresh itself
+        // This public method can be triggered from the Promo Image's parent
+        // with a BroadcastMessage("PromoRefresh") call
+        public void PromoRefresh()
+        {
+            Debug.Log("Refreshing Promo Image : " + promoLocation);
+            Start();
+        }
+
+
+        // Use this for initialization
+        void Start()
+        {
+            // Check for a Promo Image from deltaDNA Engage
+            // if the decision point and promo location are set
+            if (string.IsNullOrEmpty(decisionPoint))
+            {
+                Debug.Log("Decision Point not Set");
+                return;
+            }
+                               
+            if(string.IsNullOrEmpty(promoLocation))
+            {
+                Debug.Log("Promo Slot Location Name not Set");               
+            }
+
+            /* Will need something here to prevent Engage requests
+             * before the SDK has started.
+            do
+            {
+               StartCoroutine(Wait(1.0f));  // Don't and make Engage requests until SDK has started.
+            } while (!DDNA.Instance.isActiveAndEnabled);
+            */
+
+            Debug.Log("Check Engage for Promo Image for " + promoLocation);
+            promoCheck();
+        }
+
+        // Make an Engage In-Game campaign request
+        // To check for a promo image for this promo location
+        void promoCheck()
+        {
+            var engagement = new Engagement(decisionPoint)
+                .AddParam("promoLocation", promoLocation);
+
+            // Make request
+            DDNA.Instance.RequestEngagement(engagement, (response) => {
+
+                // Check Response
+                if (response != null && response.StatusCode == 200)
+                {
+                    // Get Image info from Engage Response
+                    FetchPromoImage(response);
+                }
+
+            }, (exception) => {
+                Debug.Log("Engage reported an error: " + exception.Message);
+            });
+
+        }
+
+
+        // Get image info from Engage Response
+        void FetchPromoImage(Engagement response)
+        {
+            // Check for Image Object
+            if(response.JSON.ContainsKey("image"))
+            {
+                JSONObject image = response.JSON["image"] as JSONObject;
+
+                // Check for Image URL
+                if (image.ContainsKey("url"))
+                {
+                    string imageUrl = image["url"] as string;
+                    // Download Image from URL
+                    StartCoroutine(LoadResourceCoroutine(imageUrl));                   
+                }                
+            }
+
+            // TODO - Add logic to download additional game parmaters from Engage
+            // To handle deeplinks, rewards, promo durations, expiry ...
+        }
+
+        // Download Image from URL to a Texture
+        private IEnumerator LoadResourceCoroutine(string url)
+        {
+            // Download Image from URL in to a Texture
+            #if UNITY_2017_1_OR_NEWER
+            using (var www = UnityWebRequestTexture.GetTexture(url))
+            {
+                #if UNITY_2017_2_OR_NEWER
+                    yield return www.SendWebRequest();
+                #else
+                yield return www.Send();
+                #endif
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Logger.LogWarning("Failed to load resource " + url + " " + www.error);
+                }
+                else
+                {
+                    downloadedImage = DownloadHandlerTexture.GetContent(www);
+                }                
+            }
+            #else
+                WWW www = new WWW(url);
+
+                yield return www;
+
+                if (www.error == null) {
+                    www.LoadImageIntoTexture(downloadedImage);
+                } else {
+                    Logger.LogWarning("Failed to load resource "+url+" "+www.error);
+                }                
+            #endif
+
+            // Create Sprite from Texture and apply to Promo Image
+            if (downloadedImage != null && promoImage != null)
+            {
+                Debug.Log("Updating " + promoLocation); 
+                promoImage.sprite = Sprite.Create(downloadedImage, new Rect(0.0f, 0.0f, downloadedImage.width, downloadedImage.height), new Vector2(0.0f, 0.0f), 1.0f);
+            }
+        }
+
+
+
+
+
+
+        // Utility method to force dealy
+        IEnumerator Wait(float duration)
+        {
+            yield return new WaitForSeconds(duration);   //Wait
+        }
+    }
+}
--- Assets/Scripts/PromoImagesTutorial.cs
+++ Assets/Scripts/PromoImagesTutorial.cs
@@ -6,14 +6,22 @@ using DeltaDNA;
 
 namespace TutoralPromoImages
 {
+    // Promo image tutorial using Engage In-Game campaigns containing image messages
+    // to populate Promo Image banners in various fixed slots in UI. 
+    // Promo Images are Prefabs that can be positioned and sized by game designer
+    // But their content and targeting can be controlled by marketer from deltDNA Platform.
+
+    // Notes :: MVP , still to apply final Promo Image content (images and deeplinks, rewards, challenges etc..)
+    // All Campaigns and actions used by this tutorial are on the LIVE environment of the deltaDNA Demo game
+    // They all reside on the "promoCheck" decision point.
+
     public class PromoImagesTutorial : MonoBehaviour
     {
         public const string CLIENT_VERSION = "0.0.01";
 
-        public Text lblUserID; 
+        public Text lblUserID;  // Displays UserID
 
-        // Use this for initialization
-        void Start()
+        private void Awake()
         {
             // Enter additional configuration here
             DDNA.Instance.ClientVersion = CLIENT_VERSION;
@@ -26,14 +34,30 @@ namespace TutoralPromoImages
                 "https://collect2674dltcr.deltadna.net/collect/api",
                 "https://engage2674dltcr.deltadna.net"
             );
+        }
 
+        // Use this for initialization
+        void Start()
+        {
             this.lblUserID.text = "UserID : " + DDNA.Instance.UserID; 
         }
 
-        // Update is called once per frame
-        void Update()
+        // Player clicks the Reset button, to wipe userID and restart as another player.
+        // Helpful for checking AB Tests
+        public void bttnReset_Click()
         {
+            DDNA.Instance.StopSDK();
+            DDNA.Instance.ClearPersistentData();
+            this.Awake();
+            this.Start();
 
         }
+
+        // Player clicks Refresh button which forces all Promo Image children to check for new images 
+        public void bttnRefresh_Click()
+        {
+            BroadcastMessage("PromoRefresh"); 
+        }
+
     }
 }
